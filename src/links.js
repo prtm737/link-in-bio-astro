1b20e2acfd5c0f4f38a10c724e1ac2f66da75655
Pritom <pritamparashar8@gmail.com>
2022-08-23 23:02:22 +0530

    first commit

---
+++ src/links.js
@@ -0,0 +1,50 @@
+export const mainContent = {
+    imgSrc: `pfp.png`,
+    name: `Pritom Parashar`,
+    tagline: `Full Stack Developer`
+  };
+  
+export const socialLinks = [
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter  ',
+    url: 'https://twitter.com/itsprtm',
+  },
+  {
+    title: 'GitHub',
+    icon: 'mdi:github',
+    url: 'https://github.com/prtm737',
+  },
+    {
+      title: 'Instagram',
+      icon: 'mdi:instagram',
+      url: 'https://www.instagram.com/pritom_parashar/',
+    },
+    
+    {
+      title: 'YouTube',
+      icon: 'mdi:youtube',
+      url: 'https://www.youtube.com/channel/UC_suPFx5sSFU0mTCKEKqQEg',
+    },
+    
+  ]
+  
+  export const links = [
+    {
+      text: 'Portfolio',
+      url: 'https://prtm-portfolio-seven.vercel.app/',
+      featured: true,
+    },
+    {
+      text: 'Linkedin',
+      url: 'https://www.linkedin.com/in/pritom-parashar-5a2636117/',
+    },
+    {
+      text: 'Twitter',
+      url: 'https://twitter.com/itsprtm',
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/prtm737'
+    }
+  ]
